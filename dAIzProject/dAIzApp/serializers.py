2bd7107d5fb7c576c3b3e413165cf4746fae4c79
jaehee <hjk0639@pukyong.ac.kr>
2022-01-24 16:54:31 +0900

    User, Diary, Ai 시리얼라이저 추가

--- dAIzProject/dAIzApp/serializers.py
+++ dAIzProject/dAIzApp/serializers.py
@@ -1,7 +1,32 @@
 from rest_framework import serializers
-from .models import Review
+from .models import Review, User, Diary, Ai
+from rest_framework_simplejwt.tokens import RefreshToken
 
 class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = Review
         fields = ('id', 'title', 'content', 'updated_at')
+        
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = "__all__"
+
+class CustomTokenRefreshSerializer(serializers.Serializer):
+    refresh_token = serializers.CharField()
+
+    def validate(self, attrs):
+        refresh = RefreshToken(attrs['refresh_token'])
+        data = {'access_token': str(refresh.access_token)}
+
+        return data
+
+class DiarySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Diary
+        fields = "__all__"
+
+class AiSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ai
+        fields = "__all__"
