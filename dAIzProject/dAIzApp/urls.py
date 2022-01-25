60afae0220965fa3f76d8d92099c43caf8ee6206
jaehee <hjk0639@pukyong.ac.kr>
2022-01-25 19:34:10 +0900

    Feat: AI 응답 urls 등록

--- dAIzProject/dAIzApp/urls.py
+++ dAIzProject/dAIzApp/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path, include
 from rest_framework import urlpatterns
 from rest_framework.urlpatterns import format_suffix_patterns
-from .views import DiaryList, DiaryDetail
+from .views import DiaryList, DiaryDetail, AiDetail
 
 urlpatterns = [
     path('signup/', include('dj_rest_auth.registration.urls')), # 회원가입
     
     path('diary/', DiaryList.as_view()),
     path('diary/<int:pk>/', DiaryDetail.as_view()),
+    path('ai/<int:pk>/', AiDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
