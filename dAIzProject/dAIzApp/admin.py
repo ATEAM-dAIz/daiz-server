7205a7ad5c3ee6afade5234fd542f037db805124
Jsgithubchannel <jsschannel99@gmail.com>
2022-03-05 21:47:49 +0900

    fix: be able to delete user on Admin page

--- dAIzProject/dAIzApp/admin.py
+++ dAIzProject/dAIzApp/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
 from .models import User, Diary, Ai
 
+from rest_framework_simplejwt.token_blacklist import models
+from rest_framework_simplejwt.token_blacklist.admin import OutstandingTokenAdmin
+
+class NewOutstandingTokenAdmin(OutstandingTokenAdmin):
+
+    def has_delete_permission(self, *args, **kwargs):
+        return True
+
+
+admin.site.unregister(models.OutstandingToken)
+admin.site.register(models.OutstandingToken, NewOutstandingTokenAdmin)
+
+
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Diary)
